f33ede217c41d359ed3dc51b7d312fa99a88aae1
Дмитрий <dmitriy.lim@gmail.com>
2022-04-12 18:57:16 +0300

    Test

---
+++ src/main/java/ru/geekbrains/lim/task1/HomeWorkApp.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.lim.task1;
+
+import java.util.Arrays;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+}
